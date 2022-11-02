bb07e98d31616420828d12613cc38f49bad09612
Yash <58439868+yash-markad@users.noreply.github.com>
2022-11-03 00:03:19 +0530

    Create esp_tx.ino

---
+++ esp_tx.ino
@@ -0,0 +1,94 @@
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+// REPLACE WITH YOUR ESP RECEIVER'S MAC ADDRESS
+uint8_t broadcastAddress1[] = {0x30,0xC6,0xF7,0x22,0x04,0x90};
+
+const int xInput = 34;
+const int yInput = 35;
+
+
+// initialize minimum and maximum Raw Ranges for each axis
+int RawMin = 0;
+int RawMax = 1023;
+
+// Take multiple samples to reduce noise
+const int sampleSize = 10;
+
+
+typedef struct test_struct {
+  int x;
+  int y;
+} test_struct;
+
+test_struct test;
+
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  char macStr[18];
+  Serial.print("Packet to: ");
+  // Copies the sender mac address to a string
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
+  Serial.print(" send status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+ 
+void setup() {
+  Serial.begin(115200);
+ 
+  WiFi.mode(WIFI_STA);
+ 
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  esp_now_register_send_cb(OnDataSent);
+   
+  // register peer
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  // register first peer  
+  memcpy(peerInfo.peer_addr, broadcastAddress1, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+int ReadAxis(int axisPin)
+{
+  long reading = 0;
+  analogRead(axisPin);
+  delay(1);
+  for (int i = 0; i < sampleSize; i++)
+  {
+  reading += analogRead(axisPin);
+  }
+  return reading/sampleSize;
+}
+
+void loop() {
+  int xRaw = ReadAxis(xInput);
+  int yRaw = ReadAxis(yInput);
+
+  // Convert raw values to 'milli-Gs"
+  long xScaled = map(xRaw, RawMin, RawMax, -3000, 3000);
+  long yScaled = map(yRaw, RawMin, RawMax, -3000, 3000);
+  test.x = xScaled;
+  test.y = yScaled;
+ 
+  esp_err_t result = esp_now_send(0, (uint8_t *) &test, sizeof(test_struct));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(200);
+}
